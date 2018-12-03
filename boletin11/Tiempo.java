e0eec9279da780ae18f50d00191bb961d57522ed
igonzaleziglesias <igonzaleziglesias@nautilus5.danielcastelao.org>
2018-12-03 12:00:09 +0100

    modificado

--- boletin11/Tiempo.java
+++ boletin11/Tiempo.java
@@ -8,11 +8,11 @@ public class Tiempo {
 
     private Date fechaIni;
     private Date fechaFin;
-    private final String fraseCorrecta = "A documentación e todo aquel conxunto de manuais impresos ou en formato dixital que explica unha aplicacion informatica";
+    private final String fraseCorrecta = "A documentacion e todo aquel conxunto de manuais impresos ou en formato dixital que explica unha aplicacion informatica";
     private String frase;
 
     public Tiempo() {
-        String[] opciones = {"INICIAR"};
+        /*String[] opciones = {"INICIAR"};
         int opcion = JOptionPane.showOptionDialog(
                 null,
                 "DESEA INICIAR?",
@@ -32,17 +32,26 @@ public class Tiempo {
                     fechaFin = new Date();
                 } while (!frase.equalsIgnoreCase(fraseCorrecta));
 
-        }
+        }*/
 
+        
+        JOptionPane.showMessageDialog(null, "INICIAR?");
+        do {
+            fechaIni = new Date();
+            frase = JOptionPane.showInputDialog("ESCRIBE A SEGUINTE FRASE: \n" + fraseCorrecta);
 
-            long transcurrido = fechaFin.getTime() - fechaIni.getTime();
-            long segundos = (transcurrido / 1000) % 60;
-            long minutos = (transcurrido / (60 * 1000)) % 60;
-            long horas = (transcurrido / (3600 * 1000)) % 60;
+            fechaFin = new Date();
+        } while (!frase.equalsIgnoreCase(fraseCorrecta));
+        
+        
+        long transcurrido = fechaFin.getTime() - fechaIni.getTime();
+        long segundos = (transcurrido / 1000) % 60;
+        long minutos = (transcurrido / (60 * 1000)) % 60;
+        long horas = (transcurrido / (3600 * 1000)) % 60;
 
-            DecimalFormat formato = new DecimalFormat("00");
+        DecimalFormat formato = new DecimalFormat("00");
 
-            JOptionPane.showMessageDialog(null, "TARDACHES " + formato.format(horas) + "-" + formato.format(minutos) + "-" + formato.format(segundos) + " TEMPO EN ESCRIBIR A FRASE");
+        JOptionPane.showMessageDialog(null, "TARDACHES " + formato.format(horas) + "-" + formato.format(minutos) + "-" + formato.format(segundos) + " TEMPO EN ESCRIBIR A FRASE");
 
     }
 }
